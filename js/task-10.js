5c51b7e6f27fbf01215920c0bd8def127870939b
Heinti <olegukraine33@gmail.com>
2022-11-06 19:57:45 +0200

    works done

--- js/task-10.js
+++ js/task-10.js
@@ -1,3 +1,48 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const refs = {
+input: document.querySelector('input'),
+form: document.querySelector('#control'),
+boxes: document.querySelector('#boxes'),
+create: document.querySelector('button[data-create]'),
+destroy: document.querySelector('button[data-destroy]')
+}
+
+let amountBoxes = 0;
+
+refs.input.addEventListener('input', getNumber)
+function getNumber(event){
+amountBoxes = event.currentTarget.value
+}
+
+function createBoxes(amount){
+  const boxesArr = [];
+  let size = 30;
+
+  for (let i = 0; i < amount; i+= 1) {
+    const newBox = document.createElement('div')
+    newBox.style.width = `${size}px`
+    newBox.style.height = `${size}px`
+    newBox.style.backgroundColor = getRandomHexColor()
+    
+    size += 10
+    boxesArr.push(newBox)
+    
+    console.log(boxesArr)
+  
+  }
+  boxes.append(...boxesArr)
+}
+
+refs.create.addEventListener('click', createElements)
+function createElements(){
+  createBoxes(amountBoxes)
+  
+}
+
+refs.destroy.addEventListener('click', destroyElements)
+function destroyElements(){
+  refs.boxes.innerHTML = ""
+}
